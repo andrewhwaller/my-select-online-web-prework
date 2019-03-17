44c23a51fcb20a0db6d15fd20bc2d26328c2dd2b
Andrew Waller <andrewhwaller@gmail.com>
2019-03-17 22:33:24 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,10 @@
 def my_select(collection)
- # your code here!
+  counter = 0
+  selection = []
+  while counter < array.length
+      selection << yield(array[counter])
+      counter += 1
+    end
+    collection
+  end
 end
